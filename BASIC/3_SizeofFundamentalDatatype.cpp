c0e72d01001e5d29888742197a2f17e5118f4443
Vidhi M Parmar <vidhi28092002@gmail.com>
2026-06-08 03:13:14 -0700

    Add files via upload

---
+++ BASIC/3_SizeofFundamentalDatatype.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+// #include <stddef.h>
+using namespace std;
+
+int main(){
+cout<<"size of int : "<<sizeof(int)<<endl;
+cout<<"size of char :"<<sizeof(char)<<endl;
+cout<<"size of float :"<<sizeof(float)<<endl;
+cout<<"size of double :"<<sizeof(double)<<endl;
+cout<<"size of bool :"<<sizeof(bool)<<endl;
+cout<<"size of void :"<<sizeof(void)<<endl;
+cout<<"size of short :"<<sizeof(short)<<endl;
+cout<<"size of long :"<<sizeof(long)<<endl;
+
+
+    return 0;
+}
